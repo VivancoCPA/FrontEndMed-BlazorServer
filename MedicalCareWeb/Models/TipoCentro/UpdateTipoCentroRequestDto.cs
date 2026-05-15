5c5b6428225042d8cd879d2e0cd61c59884e0cee
Kandor25 <vivancoantoniop@gmail.com>
2026-05-15 01:16:36 -0500

    TipoCentro1

--- MedicalCareWeb/Models/TipoCentro/UpdateTipoCentroRequestDto.cs
+++ MedicalCareWeb/Models/TipoCentro/UpdateTipoCentroRequestDto.cs
@@ -2,6 +2,6 @@
 {
     public class UpdateTipoCentroRequestDto: CreateTipoCentroRequestDto
     {
-        public int id { get; set; }
+        public int Id { get; set; }
     }
 }
--- MedicalCareWeb/Services/TipoCentroService.cs
+++ MedicalCareWeb/Services/TipoCentroService.cs
@@ -9,9 +9,14 @@ namespace MedicalCareWeb.Services;
 
 public class TipoCentroService(HttpClient _http) : ITipoCentroService
 {
-    public Task<ApiResponse<bool>> CreateTipoCentroAsync(CreateTipoCentroRequestDto request)
+    public async Task<ApiResponse<bool>> CreateTipoCentroAsync(CreateTipoCentroRequestDto request)
     {
-        throw new NotImplementedException();
+        var response = await _http.PostAsJsonAsync("center-types", request);
+        if (response.IsSuccessStatusCode)
+            return new ApiResponse<bool>(true, true, string.Empty);
+
+        var error = await Helper.LeerErrorAsync(response);
+        return new ApiResponse<bool>(false, false, error);
     }
 
     public async Task<IEnumerable<TipoCentroDto>> GetAllTipoCentroAsync(EnumStatus status) 
@@ -31,9 +36,39 @@ public class TipoCentroService(HttpClient _http) : ITipoCentroService
         }
         return await _http.GetFromJsonAsync<IEnumerable<TipoCentroDto>>(url) ?? Enumerable.Empty<TipoCentroDto>();
     }
-    public Task<PaginatedResultDto<TipoCentroDto>> GetTipoCentroPaged(ListedPagedDto paginacion)
+    //
+    public async Task<PaginatedResultDto<TipoCentroDto>> GetTipoCentroPaged(ListedPagedDto paginacion)
     {
-        throw new NotImplementedException();
+        var url = $"center-types/paged?page={paginacion.Page}&pageSize={paginacion.PageSize}";
+        //Console.WriteLine(url);
+        if (!string.IsNullOrWhiteSpace(paginacion.Search))
+            url += $"&search={Uri.EscapeDataString(paginacion.Search)}";// Asegúrate de escapar el término de búsqueda para evitar problemas con caracteres especiales
+
+        if (!string.IsNullOrWhiteSpace(paginacion.SortBy))
+            url += $"&sortBy={paginacion.SortBy}&sortDesc={paginacion.SortDesc.ToString().ToLower()}";
+
+        var response = await _http.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadFromJsonAsync<PaginatedResultDto<TipoCentroDto>>();
+            return data ?? new PaginatedResultDto<TipoCentroDto>(
+                Items: [],
+                Page: paginacion.Page,
+                PageSize: paginacion.PageSize,
+                TotalCount: 0,
+                Success: true,
+                ErrorMessage: "");
+        }
+        // ❌ leer error backend
+        var error = await Helper.LeerErrorAsync(response);
+        return new PaginatedResultDto<TipoCentroDto>(
+            Items: [],
+            TotalCount: 0,
+            Page: 0,
+            PageSize: 0,
+            Success: false,
+            ErrorMessage: error
+        );
     }
 
     public Task<ApiResponse<bool>> ToggleTipoCentroAsync(int id)
@@ -41,8 +76,15 @@ public class TipoCentroService(HttpClient _http) : ITipoCentroService
         throw new NotImplementedException();
     }
 
-    public Task<ApiResponse<bool>> UpdateTipoCentroAsync(int id, UpdateTipoCentroRequestDto request)
+    public async Task<ApiResponse<bool>> UpdateTipoCentroAsync(int id, UpdateTipoCentroRequestDto request)
     {
-        throw new NotImplementedException();
+        var response = await _http.PutAsJsonAsync($"center-types/{id}", request);
+        if (response.IsSuccessStatusCode)
+        {
+            return new ApiResponse<bool>(true, true, string.Empty);
+        }
+        //ERROR
+        var error = await Helper.LeerErrorAsync(response);
+        return new ApiResponse<bool>(false, false, error);
     }
 }
